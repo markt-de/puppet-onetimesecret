4ef74f14c97d3ce1a910f72dc58220c3a408c510
Frank Wall <fw@moov.de>
2020-11-05 23:03:31 +0100

    fix empty $host

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [2.0.1] - 2020-11-05
+
+### Fixed
+* Fix empty `$host`, set default value to FQDN 
+
 ## [2.0.0] - 2020-11-05
 This release introduces several breaking changes. Redis is no longer
 maintained by this module, as a result, the Redis configuration is not
@@ -58,6 +63,7 @@ Afterwards change `$redis_options` to be compatible with your new Redis instance
 ## 1.0.0 - 2016-12-18
 * Initial release
 
-[Unreleased]: https://github.com/fraenki/puppet-onetimesecret/compare/v2.0.0...HEAD
+[Unreleased]: https://github.com/fraenki/puppet-onetimesecret/compare/v2.0.1...HEAD
+[2.0.1]: https://github.com/fraenki/puppet-onetimesecret/compare/v2.0.0...v2.0.1
 [2.0.0]: https://github.com/fraenki/puppet-onetimesecret/compare/v1.0.1...v2.0.0
 [1.0.1]: https://github.com/fraenki/puppet-onetimesecret/compare/v1.0.0...v1.0.1
--- manifests/config.pp
+++ manifests/config.pp
@@ -10,25 +10,25 @@ class onetimesecret::config {
 
     # Update web app defaults with real data
     $default_options_override = {
-      'site' => {
-        'host' => $onetimesecret::host,
-        'domain' => $onetimesecret::domain,
-        'secret' => $onetimesecret::secret,
+      site => {
+        host => $onetimesecret::host,
+        domain => $onetimesecret::domain,
+        secret => $onetimesecret::secret,
       },
-      'redis' => {
-        'dir' => $onetimesecret::data_dir,
-        'uri' => $redis_uri,
+      redis => {
+        dir => $onetimesecret::data_dir,
+        uri => $redis_uri,
       },
     }
     $default_options = deep_merge($onetimesecret::default_options, $default_options_override)
 
     # Check if defaults should be included
     if ( $onetimesecret::use_default_options == true ) {
-      $real_options       = deep_merge($default_options, $onetimesecret::options)
+      $real_options = deep_merge($default_options, $onetimesecret::options)
     }
     # do not include defaults
     else {
-      $real_options       = $onetimesecret::options
+      $real_options = $onetimesecret::options
     }
 
     file { $onetimesecret::config_file:
