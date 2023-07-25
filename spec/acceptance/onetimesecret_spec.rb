c0e050e8196dbdb3290588f8756f4644b828891c
Frank Wall <fw@moov.de>
2023-07-25 13:37:30 +0200

    add basic tests, enable GitHub Actions

---
+++ spec/acceptance/onetimesecret_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper_acceptance'
+
+describe 'onetimesecret' do
+  let(:onetimesecret_password) { 'SomeHardToGuessRandomCharacters' }
+  let(:onetimesecret_version) { 'v0.9.2' }
+  let(:redis_password) { 'AnotherGoodPassword' }
+
+  context 'with default parameters' do
+    let(:pp) do
+      %(class { 'onetimesecret':
+        redis_password => '#{redis_password}',
+        secret         => '#{onetimesecret_password}',
+        version        => '#{onetimesecret_version}',
+      })
+    end
+
+    it { apply_manifest(pp, catch_failures: true) }
+  end
+end
---
+++ spec/classes/onetimesecret_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+ONETIMESECRET_PASSWORD = 'SomeHardToGuessRandomCharacters'.freeze
+ONETIMESECRET_VERSION = 'v0.9.2'.freeze
+REDIS_PASSWORD = 'AnotherGoodPassword'.freeze
+
+describe 'onetimesecret' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context 'with default parameters' do
+        let :params do
+          {
+            redis_password: REDIS_PASSWORD.to_s,
+            secret: ONETIMESECRET_PASSWORD.to_s,
+            version: ONETIMESECRET_VERSION.to_s,
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
+  end
+end
