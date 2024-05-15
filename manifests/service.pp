c1bc29fba9d94bcdb569295e94a84228a8e3dad9
Frank Wall <fw@moov.de>
2024-05-15 13:58:06 +0200

    fix working directory for systemd service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,13 +2,20 @@
 # @api private
 class onetimesecret::service {
   if $onetimesecret::manage_service == true {
+    $working_dir = $onetimesecret::version ? {
+      /^v(\d+\..*)$/ => "${onetimesecret::install_dir}/onetimesecret-${1}",
+      default        => "${onetimesecret::install_dir}/onetimesecret-${onetimesecret::version}",
+    }
+
     # Install service file.
     file { $onetimesecret::service_file:
       ensure  => file,
       owner   => 'root',
       group   => 0,
       mode    => '0644',
-      content => epp($onetimesecret::service_template),
+      content => epp($onetimesecret::service_template,{
+        working_dir => $working_dir,
+      }),
     }
 
     # Enable service.
--- spec/classes/onetimesecret_spec.rb
+++ spec/classes/onetimesecret_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 ONETIMESECRET_PASSWORD = 'SomeHardToGuessRandomCharacters'.freeze
 ONETIMESECRET_VERSION = 'v0.13.0-RC7'.freeze
+ONETIMESECRET_VERSION_HASH = 'a46fdeb2274bb528516505cec8ffb3568dd47c1c'.freeze
 REDIS_PASSWORD = 'AnotherGoodPassword'.freeze
 
 describe 'onetimesecret' do
@@ -19,6 +20,19 @@ describe 'onetimesecret' do
         end
 
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_file('/etc/systemd/system/onetimesecret.service').with_content(%r{WorkingDirectory=/opt/onetimesecret-0.13.0-RC7}) }
+      end
+
+      context 'when setting version to a commit hash' do
+        let :params do
+          {
+            redis_password: REDIS_PASSWORD.to_s,
+            secret: ONETIMESECRET_PASSWORD.to_s,
+            version: ONETIMESECRET_VERSION_HASH.to_s,
+          }
+        end
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_file('/etc/systemd/system/onetimesecret.service').with_content(%r{WorkingDirectory=/opt/onetimesecret-#{ONETIMESECRET_VERSION_HASH}}) }
       end
     end
   end
